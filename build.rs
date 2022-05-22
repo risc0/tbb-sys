5b030c3c9b230e1555c3b6f320de6648a8dad570
Frank Laub <flaub@risc0.com>
2022-05-22 03:33:11 +0000

    Initial commit

---
+++ build.rs
@@ -0,0 +1,45 @@
+use std::path::{Path, PathBuf};
+
+use cxx_build::CFG;
+use glob::glob;
+
+fn main() {
+    let inc_dir = Path::new("tbb").join("include").canonicalize().unwrap();
+    let srcs: Vec<PathBuf> = glob("tbb/src/tbb/*.cpp")
+        .unwrap()
+        .map(|x| x.unwrap())
+        .collect();
+
+    CFG.exported_header_dirs = vec![&inc_dir];
+
+    let mut build = cxx_build::bridge("src/lib.rs");
+    build
+        .files(srcs)
+        .include(&inc_dir)
+        .define("__TBB_BUILD", None)
+        .define("__TBB_NO_IMPLICIT_LINKAGE", None)
+        // MSVC flags
+        .flag_if_supported("/std:c++17")
+        .flag_if_supported("/Zc:preprocessor")
+        .flag_if_supported("/EHsc")
+        // GCC/Clang flags
+        .flag_if_supported("-w")
+        .flag_if_supported("-std=c++17")
+        .warnings(false);
+
+    if cfg!(target_os = "macos") {
+        build.define("_XOPEN_SOURCE", None);
+    }
+
+    if cfg!(target_arch = "x86_64") {
+        build.define("__TBB_USE_ITT_NOTIFY", None);
+    } else {
+        build.define("USE_PTHREAD", None);
+    }
+
+    if cfg!(all(target_os = "linux", target_arch = "x86_64")) {
+        build.flag_if_supported("-mwaitpkg");
+    }
+
+    build.compile("tbb");
+}
